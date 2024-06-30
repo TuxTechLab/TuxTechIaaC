16cf19acefabd26eb41e5d36b55f452ba0e250bc
Arindam <arindamtanti123@gmail.com>
2024-06-30 10:34:19 +0530

    Code ( Signed Commit ) | Adding Functinatlity to Docusaurus Check Shell Script to verify docusaurus npm pacakge is installed or not. Passing, hence adding in main.

--- scripts/check_docusaurus_dependencies.sh
+++ scripts/check_docusaurus_dependencies.sh
@@ -4,7 +4,8 @@
 REQUIRED_NODE_VERSION="v20.15.0"
 REQUIRED_NPM_VERSION="10.7.0"
 REQUIRED_NPX_VERSION="10.7.0"
-# REQUIRED_NODE_PACKAGE="@docusaurus/core"
+REQUIRED_DOCUSAURUS_VERSION="3.4.0"
+REQUIRED_NPM_PACKAGE="@docusaurus/core"
 
 # Help function
 help() {
@@ -54,6 +55,45 @@ check_npx_version() {
     fi
 }
 
+check_docusaurus_version(){
+    DOCUSAURUS_VERSION=$(cd ../www/tuxtechiaac/ && npx docusaurus -V)
+    if version_ge "$DOCUSAURUS_VERSION" "$REQUIRED_DOCUSAURUS_VERSION"; then
+        echo "Docusaurus Version is $DOCUSAURUS_VERSION (required: $REQUIRED_DOCUSAURUS_VERSION)"
+    else
+        echo "Docusaurus is either not installed or having issues, (required) $REQUIRED_DOCUSAURUS_VERSION"
+        echo "Printing : www/tuxtechiaac/package.json"
+        echo ""
+        echo "-----------------------------------------"
+        cat "../www/tuxtechiaac/package.json"
+        echo "-----------------------------------------"
+        echo "Please check: https://docusaurus.io/docs/installation."
+        exit 1
+    fi
+}
+
+check_docusaurus_installed() {
+    # Change directory to the Docusaurus project
+    cd ../www/tuxtechiaac
+
+    # Check if Docusaurus is installed
+    if npm ls --depth 1 | grep -q '@docusaurus/core'; then
+        DOCUSAURUS_VERSION=$(npm ls @docusaurus/core --depth 0 | grep @docusaurus/core | awk -F'@' '{print $3}')
+        echo "Docusaurus Version is $DOCUSAURUS_VERSION (required: $REQUIRED_DOCUSAURUS_VERSION)"
+    else
+        echo "Docusaurus is either not installed or having issues, (required) $REQUIRED_DOCUSAURUS_VERSION"
+        echo "Printing: ../www/tuxtechiaac/package.json"
+        echo ""
+        echo "-----------------------------------------"
+        cat "../www/tuxtechiaac/package.json"
+        echo "-----------------------------------------"
+        echo "Please check: https://docusaurus.io/docs/installation."
+        exit 1
+    fi
+
+    # Change back to the original directory
+    cd - > /dev/null
+}
+
 # Main function
 main() {
     # Parse command line arguments
@@ -64,6 +104,10 @@ main() {
     check_node_version || exit 1
     check_npm_version  || exit 1
     check_npx_version  || exit 1
+    check_docusaurus_version || exit 1
+    check_docusaurus_installed || exit 1
+    echo "Passed : Dependency Check | TuxTechIaaC Docusaurus Website"
+    echo "Next To Start Server view ./manage_docusaurus_server.sh --help"
     exit 0
 }
 
